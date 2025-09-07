4951477143361e69e1cdd65432997d0fadacc4ff
This-Is-Ko <52279273+This-Is-Ko@users.noreply.github.com>
2025-09-07 16:28:48 +1000

    Updated build jar

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@
 FROM amd64/eclipse-temurin:21.0.4_7-jdk-alpine
 WORKDIR /app
 VOLUME /tmp
-ARG JAR_FILE=target/*.jar
+ARG JAR_FILE
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java", "-jar", "app.jar", "--spring.profiles.active=prod"]
--- src/main/java/org/trakket/TrakketApplication.java
+++ src/main/java/org/trakket/TrakketApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SportsTrackerApplication {
+public class TrakketApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SportsTrackerApplication.class, args);
+        SpringApplication.run(TrakketApplication.class, args);
     }
 }
