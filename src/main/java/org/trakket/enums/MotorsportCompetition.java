2daccb17b44e0d99f0e3aab0280580bf4ef4b18c
This-Is-Ko <52279273+This-Is-Ko@users.noreply.github.com>
2025-09-14 13:46:16 +1000

    Updated MotorsportCompetition returned json

--- src/main/java/org/trakket/enums/MotorsportCompetition.java
+++ src/main/java/org/trakket/enums/MotorsportCompetition.java
@@ -1,7 +1,10 @@
 package org.trakket.enums;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
 
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum MotorsportCompetition {
     FORMULA_ONE("Formula One"),
     WORLD_ENDURANCE_CHAMPIONSHIP("World Endurance Championship"),
@@ -14,8 +17,4 @@ public enum MotorsportCompetition {
         this.displayName = displayName;
     }
 
-    @JsonValue
-    public String getDisplayName() {
-        return displayName;
-    }
 }
